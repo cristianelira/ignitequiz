09d097ab3ad055b0349909e1b3048c2031e38457
cristianelira <79479647+cristianelira@users.noreply.github.com>
2024-08-19 12:31:57 -0300

    feat: add microinterações com o Gesture Handler

--- src/components/QuizHeader/styles.ts
+++ src/components/QuizHeader/styles.ts
@@ -1,11 +1,10 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native'
 
-import { THEME } from '../../styles/theme';
+import { THEME } from '../../styles/theme'
 
 export const styles = StyleSheet.create({
   container: {
-    width: '100%',
-    marginBottom: 21,
+    width: '100%'
   },
   header: {
     width: '100%',
@@ -13,17 +12,17 @@ export const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     marginBottom: 8,
-    marginTop: 40,
+    marginTop: 40
   },
   title: {
     fontFamily: THEME.FONTS.BOLD,
     color: THEME.COLORS.GREY_100,
-    fontSize: 16,
+    fontSize: 16
   },
   question: {
     color: '#C4C4CC'
   },
   length: {
     color: '#C4C4CC'
-  },
-});
+  }
+})
--- src/screens/History/styles.ts
+++ src/screens/History/styles.ts
@@ -1,13 +1,29 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native'
 
-import { THEME } from '../../styles/theme';
+import { THEME } from '../../styles/theme'
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: THEME.COLORS.GREY_800,
+    backgroundColor: THEME.COLORS.GREY_800
   },
   history: {
-    padding: 32,
+    flexGrow: 1,
+    padding: 32
+  },
+  swipeableContainer: {
+    width: '100%',
+    height: 90,
+    marginBottom: 12,
+    backgroundColor: THEME.COLORS.DANGER_LIGHT,
+    borderRadius: 6
+  },
+  swipeableRemove: {
+    width: 90,
+    height: 90,
+    borderRadius: 6,
+    backgroundColor: THEME.COLORS.DANGER_LIGHT,
+    alignItems: 'center',
+    justifyContent: 'center'
   }
-});
+})
